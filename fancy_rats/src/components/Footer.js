28c8b82fffb1d86f81d05254b99cf30e4b4007cc
Dana Varahi <dana_varahi@protonmail.com>
2021-01-04 17:25:10 +0000

    Responsive from max width 600

--- fancy_rats/src/components/Footer.js
+++ fancy_rats/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
             <h4>Project by Dana Varahi</h4>
             <h4>Header Image: <a href="https://publicdomainvectors.org/">publicdomainvectors.org</a></h4>
             <h4>Rat Icons: <a href="http://www.woodcutter.es/">www.woodcutter.es</a></h4>
-            <img src="images/rat-font-credit.png"></img>
+            <img src="images/rat-font-credit.png" alt="woodcutter rats promo image"></img>
         </div>
     )
 }
