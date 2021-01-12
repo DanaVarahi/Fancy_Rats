ed9ade8d794a0557744439bbfbf069ca673a72f3
Dana Varahi <dana_varahi@protonmail.com>
2021-01-12 20:05:31 +0000

    Created Rat King component

--- fancy_rats/src/App.js
+++ fancy_rats/src/App.js
@@ -5,6 +5,7 @@ import RatSort from './components/RatSort'
 import Header from './components/Header'
 import NewRat from './components/NewRat'
 import Footer from './components/Footer'
+import RatKing from './components/RatKing'
 
 function App() {
 
@@ -57,29 +58,34 @@ function App() {
   }
 
 
-  rats.sort((rat1, rat2) => {
-    if (sort){
-  
-      if(sort === 'age'){
-        const rat1YearsToMonths = rat1.age.years * 12
-        const rat2YearsToMonths = rat2.age.years * 12
-        const rat1Age = rat1YearsToMonths + rat1.age.months
-        const rat2Age = rat2YearsToMonths + rat2.age.months
-        if(rat1Age < rat2Age){
-          return -1
+  const ratSort = (rats, sort) => {
+    rats.sort((rat1, rat2) => {
+      if (sort){
+    
+        if ( sort === 'age' ){
+          const rat1YearsToMonths = rat1.age.years * 12
+          const rat2YearsToMonths = rat2.age.years * 12
+          const rat1Age = rat1YearsToMonths + rat1.age.months
+          const rat2Age = rat2YearsToMonths + rat2.age.months
+
+          if ( rat1Age < rat2Age ){
+            return -1
+          } else {
+            return 1
+          }
         } else {
-          return 1
+          if ( rat1[sort].toLowerCase() < rat2[sort].toLowerCase() ) {
+            return -1
+          } else {
+            return 1
+          }
         }
-      } else {
-        if(rat1[sort].toLowerCase() < rat2[sort].toLowerCase()){
-          return -1
-        } else {
-          return 1
-        }
-      }
-    }   
-    return 0
-  })
+      }   
+      return 0
+    })
+  } 
+
+  ratSort(rats, sort)
 
  const addNewRat = (submittedRat) => {
    postData('http://localhost:3000/api/rats', submittedRat)
@@ -89,6 +95,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <RatKing rats={rats}></RatKing>
       <NewRat submitNewRat={ (newRat) => addNewRat(newRat)}></NewRat>
       <RatSort   handleSortSelection={handleSortSelection} sortOptions={sortCategories}></RatSort>
       <RatList rats={rats} generateRatIcon={generateRatIcon}></RatList>
---
+++ fancy_rats/src/components/RatKing.js
@@ -0,0 +1,13 @@
+const RatKing = ({rats}) => {
+
+    const ratKingAge = 5
+
+    
+
+
+    return (
+        <p>Rat King is {ratKingAge}</p>
+    )
+}
+
+export default RatKing
