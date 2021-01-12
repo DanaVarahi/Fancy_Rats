83dfaace6068059b39b553100a496abcbb9b4896
Dana Varahi <dana_varahi@protonmail.com>
2021-01-12 21:03:53 +0000

    Adedd methods to calculate rat king age

--- fancy_rats/src/components/RatKing.js
+++ fancy_rats/src/components/RatKing.js
@@ -1,12 +1,38 @@
 const RatKing = ({rats}) => {
 
-    const ratKingAge = 5
+    const ratKingAge = () => {
 
+        let totalYears = 0
+        let totalMonths = 0
+
+        rats.forEach((rat) => {
+            
+            totalYears += rat.age.years
+            totalMonths += rat.age.months  
+            console.log("y" + totalYears)
+            console.log("m" + totalMonths)
+        })
+
+        return getAgeTotal(totalYears, totalMonths)
+    }
+
+    const getAgeTotal = (years, months) => {
+        console.log("years:" + years)
+        console.log("months:" + months)
+        let ageYears = years
+        let ageMonths = months
+
+        ageYears += Math.floor(ageMonths / 12)
+        ageMonths = ageMonths % 12
+
+        const ageTotal = ageYears + "years and " + ageMonths + "months"
+        return ageTotal
+    }
     
 
 
     return (
-        <p>Rat King is {ratKingAge}</p>
+        <p>Rat King is {ratKingAge()}</p>
     )
 }
 
